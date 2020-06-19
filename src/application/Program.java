dfb26a125ee081c26566e26586b8aff261322142
ThomasPeruch <peruchthomas@gmail.com>
2020-06-19 12:15:10 -0300

    implementação inicial

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import services.ProductService;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Playstation 4",2300.00));
+		list.add(new Product("Playstation 3",800.00));
+		list.add(new Product("Smarthphone",1500.00));
+		list.add(new Product("Notebook",1800.00));
+		list.add(new Product("Cabo HDMI",50.00));
+		list.add(new Product("Headset",200.00));
+		
+		ProductService ps = new ProductService();
+		
+		double sum  = ps.filteredSum(list);
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));	
+	}
+}
+
